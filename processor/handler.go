45072a5c411eabce8122f5f7a0a1131041d8ebd0
Richard Berg <rberg@bitwise.io>
2018-02-21 14:32:24 -0600

    Update repo name used for import
    Signed-off-by: Richard Berg <rberg@bitwise.io>

--- processor/handler.go
+++ processor/handler.go
@@ -27,7 +27,7 @@
 package processor
 
 import (
-	"sawtooth_sdk/protobuf/processor_pb2"
+	"sawtooth-go-sdk/protobuf/processor_pb2"
 )
 
 // TransactionHandler is the interface that defines the business logic for a
